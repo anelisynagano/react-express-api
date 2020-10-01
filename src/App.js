d20b6fec442f719837b9e257bc6332ee07c4de48
WildCodeSchool <anelisynagano@gmail.com>
2020-10-01 19:19:57 +0100

    start repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [students, setStudents] = useState([]);
+
+  const fetchStudents = () => {
+    fetch('http://localhost:5000/students')
+      .then(res => res.json())
+      .then(data => setStudents(data))
+  }
+
+  useEffect(fetchStudents, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hi from the frontend
     </div>
   );
 }
